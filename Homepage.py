868eec2bd9d34421172c788d8a0e05aeecc444ce
mathursanidhya <146045399+mathursanidhya@users.noreply.github.com>
2023-12-15 23:08:56 -0500

    Main File

---
+++ Homepage.py
@@ -0,0 +1,21 @@
+import streamlit as st
+ 
+# Title and description
+st.title("Algorithmic Digital Marketing Course")
+st.header("Presented by Aishwarya SVS, Arjun Janardhan, Sanidhya Mathur")
+st.header("Northeastern University")
+ 
+# Displaying the Northeastern University logo
+st.image("northeastern_logo.png", use_column_width=True)
+ 
+# Add more content or components as needed
+# You can use st.text, st.markdown, st.button, st.checkbox, st.selectbox, st.image, etc.
+ 
+# Example:
+# st.markdown("## Course Overview")
+# st.write("This course covers the fundamentals of Algorithmic Digital Marketing...")
+ 
+# You can add more sections and content as required for your course information.
+ 
+# Footer
+st.write("© 2023 Northeastern University")
